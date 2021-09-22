e3ea514027f6dbf4b749968f1ebaea9a14077b1c
dpgaire <gairhedurga13@gmail.com>
2021-09-22 20:26:40 +0545

    Sections done

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,54 @@ import Section from './Section'
 function Home() {
     return (
         <Container>
-            <Section/>
+            <Section
+                title="Model S"
+                description="Order online for Touchless Delivery"
+                backgroundImg="images/model-s.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Model Y"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="images/model-y.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Model 3"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="images/model-3.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Model X"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="images/model-x.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+              <Section
+                title="Solar Panels"
+                description="Lowest Cost Solar Panels in America"
+                backgroundImg="images/solar-panel.jpg"
+                leftBtnText="Order Now"
+                rightBtnText="Learn More"
+            />
+             <Section
+                title="Solar Roof"
+                description="Produce Clean Energy From Your Roof"
+                backgroundImg="images/solar-roof.jpg"
+                leftBtnText="Order Now"
+                rightBtnText="Learn More"
+            />
+                <Section
+                title="Accessories"
+                description=""
+                backgroundImg="images/accessories.jpg"
+                leftBtnText="Shop Now"
+            />
         </Container>
     )
 }
@@ -12,7 +59,5 @@ function Home() {
 export default Home
 
 const Container=styled.div `
-    height:100vh;
-    width:100vw;
-    background:green;
+
 `
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,13 +1,28 @@
 import React from 'react'
-
+import '../index.css'
 import styled from 'styled-components';
-function Section() {
+function Section({title,description,backgroundImg,leftBtnText,rightBtnText}) {
+    console.log(backgroundImg)
     return (
-        <Wrap>
+        <Wrap bgImg={backgroundImg}>
            <ItemText>
-               <h1>Model S</h1>
-               <p>Order online for Touchless Delivery</p>
+               <h1>{title}</h1>
+               <p>{description}</p>
            </ItemText>
+           <Buttons>
+           <ButtonGroup>
+                <LeftButton>
+                    {leftBtnText}
+                </LeftButton>
+                {rightBtnText && 
+                 <RightButton>
+                    {rightBtnText}
+                </RightButton>
+                }
+           </ButtonGroup>
+            <DownArrow src="images/down-arrow.svg"/>
+           </Buttons>
+          
         </Wrap>
     )
 }
@@ -17,11 +32,54 @@ export default Section
 const Wrap=styled.div `
     height:100vh;
     width:100vw;
-    background-image:url("/images/model-s.jpg");
+    // background-image:url('/images/model-s.jpg');
+    background-image: ${props => `url('${props.bgImg}')`};
     background-size: cover;
     background-repeat:no-repeat;
     background-position:center;
+    text-align:center;
+    display:flex;
+    flex-direction:column;
+    justify-content:space-between;
+    align-items:center;
 `
 const ItemText=styled.div `
     padding-top:100px;
+
 `
+const ButtonGroup=styled.div `
+    display:flex;
+    justify-content:center;
+    margin-bottom:30px;
+     @media(max-width:768px){
+        flex-direction:column;
+    }
+`
+const LeftButton=styled.div `
+    background-color:rgba(23,26,32,0.8);
+    height:40px;
+    width:260px;
+    color:white;
+    text-transform:uppercase;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    border-radius:100px;
+    opacity:0.85;
+    font-size:14px;
+    cursor:pointer;
+    margin:8px;
+`
+const RightButton=styled(LeftButton) `
+    background-color:white;
+    color:black;
+    opacity:0.65;
+
+`
+const DownArrow=styled.img `
+    height:40px;
+    margin-top:40px;
+    animation:animateDown infinite 1.4s;
+`
+
+const Buttons=styled.div ``
